502a7ee3613b0f42e8795a8871bdd9f979a92b50
Yeseul O <yeseulo@gmail.com>
2020-08-01 16:49:39 +0900

    Add css library cdn

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Link href="/articles">
+        <Link href='/articles'>
           <a>Article List</a>
         </Link>
+        <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+          Button
+        </button>
       </main>
 
       <footer className={styles.footer}>
